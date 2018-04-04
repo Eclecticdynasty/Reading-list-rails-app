378635dbe58f0a04124807268c6303a2f6869c0b
Eclecticdynastye <bklyngal1994@gmail.com>
2018-04-04 17:03:38 -0400

    user migration updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180404201938) do
+ActiveRecord::Schema.define(version: 20180404204519) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "workouts", force: :cascade do |t|
     t.string "workout"
