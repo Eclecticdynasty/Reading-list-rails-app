d707d6fb697ba762126c9e64ffdd4b8ddd74107c
Eclecticdynastye <bklyngal1994@gmail.com>
2018-04-04 17:08:53 -0400

    uid added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180404204519) do
+ActiveRecord::Schema.define(version: 20180404210745) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20180404204519) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "uid"
   end
 
   create_table "workouts", force: :cascade do |t|
